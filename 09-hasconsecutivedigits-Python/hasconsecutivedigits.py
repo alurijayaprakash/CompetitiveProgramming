4c26e1b3c633dfdca61c743e7ab28fc84b93a14c
alurijayaprakash <alurijayaprakash@gmail.com>
2021-08-20 18:50:33 +0530

    done

--- 09-hasconsecutivedigits-Python/hasconsecutivedigits.py
+++ 09-hasconsecutivedigits-Python/hasconsecutivedigits.py
@@ -4,4 +4,18 @@
 
 def hasconsecutivedigits(n):
 	# your code goes here
-	pass
+	n = abs(n)
+	prev = n % 10
+	n = n // 10
+	print(n,prev)
+	while n != 0:
+		rem = n % 10
+		print(n,prev)
+		if prev == rem:
+			return True
+		else:
+			prev = rem
+			n = n // 10
+	return False
+
+print(hasconsecutivedigits(14))
