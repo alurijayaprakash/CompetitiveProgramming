49171b95dbae1af4a8195e99d000d8d07f67fcb1
alurijayaprakash <alurijayaprakash@gmail.com>
2021-09-01 03:22:48 +0530

    done

--- LatinSquare.py
+++ LatinSquare.py
@@ -57,3 +57,24 @@ lst2 = [[1, 2, 3, 4],
         [4, 1, 3, 2]]
 assert(isLatinSquare(lst2) == False)
 print("All test cases passed...!")
+
+
+
+
+
+
+# def isLatinSquare(lst):
+#     a=[]
+#     h=0
+#     if len(lst)== len(lst[0]):
+#         for i in range(len(lst)):
+#             for j in range(len(lst)):
+#                 a.append(lst[i][j])
+#             break
+#     for i in range(len(lst)):
+#         for j in range(len(lst)):
+#             if lst[i][j] in a and lst[i][j] in a:
+#                 continue
+#             else:
+#                 return False
+#     return True
